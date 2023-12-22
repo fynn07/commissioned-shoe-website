31d2f3597f1cfb76114debb374a23e6a14bd711e
Fynn Borja <pewdiestuff@gmail.com>
2023-12-22 10:24:58 +0800

    Finished Partners Page

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,15 @@
 function redirectLogin(){
     window.location.href = "../pages/login.html"
 }
+
+function redirectMartens(){
+    window.open("https://www.drmartens.com/", "_blank");
+}
+
+function redirectGucci(){
+    window.open("https://www.gucci.com/", "_blank");
+}
+
+function redirectVersace(){
+    window.open("https://www.versace.com/", "_blank");
+}
