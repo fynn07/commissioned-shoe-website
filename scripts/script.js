4b93b53f1c8c3d8f0cc60ce710b7b00b71b21e81
Fynn Borja <pewdiestuff@gmail.com>
2023-12-23 12:42:31 +0800

    Finished Profile Page | Implemented Login Function

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,15 @@ function redirectGucci(){
 function redirectVersace(){
     window.open("https://www.versace.com/", "_blank");
 }
+
+function login(){
+    let try_user = document.getElementById("user").value;
+    let try_pass = document.getElementById("pass").value;
+
+    if("correct" === try_user && "correct" === try_pass){
+        window.location.href ="profile.html";
+    }
+    else{
+        document.getElementById("login-failed").innerHTML = "Login Failed";
+    }
+}
